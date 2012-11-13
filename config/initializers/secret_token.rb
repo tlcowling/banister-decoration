42fa55addc67356eac06a65f59ba4ba2e8bde79d
Tom Cowling <tom.cowling@gmail.com>
2012-11-13 16:33:35 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BanisterDecoration::Application.config.secret_token = 'bc1da76e3078c3c26e129c65e228b7f4dfe773be98e5da97725ad2b1a4db19a4a306e7d15eea543c748c59250b4e21cdf1b2b8b7debe825407acd8f9d087c53e'
